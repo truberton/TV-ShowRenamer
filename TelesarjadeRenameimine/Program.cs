a4b9f141b88a083bb520f7e4c17544164a923e1e
Gert-Andry <gertandry@gmail.com>
2017-11-27 21:54:51 +0200

    Põhiidee valmis

--- TelesarjadeRenameimine/Program.cs
+++ TelesarjadeRenameimine/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace TelesarjadeRenameimine
 {
@@ -10,6 +11,29 @@ namespace TelesarjadeRenameimine
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(@"Mis on path, kus failid on? (See peab lõppema [\], näiteks E:\TestFolder\)");
+            string path = Console.ReadLine();
+            string[] AllFiles = Directory.GetFiles(path);
+            Console.WriteLine("Mis tahad, et faili nimi oleks? (Näide: S01E*, tärni osa küsin hiljem)");
+            string nimi = Console.ReadLine();
+            Console.WriteLine("Mis osast/episoodist alustada? (See tärn)");
+            int EsimeneOsa = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Mis tüüpi failid need on? (.mp4, .avi, .wma jne)");
+            string failiTüüp = Console.ReadLine();
+
+            foreach (string file in AllFiles)
+            {
+                if (EsimeneOsa < 10)
+                {
+                    File.Move(file, path + nimi+ "0" + EsimeneOsa + failiTüüp);
+                }
+                else
+                {
+                    File.Move(file, path + nimi + EsimeneOsa + failiTüüp);
+                }
+                EsimeneOsa++;
+            }
+            Console.WriteLine(path + nimi + EsimeneOsa);
         }
     }
 }
