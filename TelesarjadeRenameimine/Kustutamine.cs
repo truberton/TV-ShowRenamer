bc294c3699fe9ca96a71548663029d6c9c6f586c
Gert-Andry <gert-andry.kaaramees@tptlive.ee>
2017-11-28 09:34:09 +0200

    Lisasin kustutamise funktsiooni

---
+++ TelesarjadeRenameimine/Kustutamine.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace TelesarjadeRenameimine
+{
+    class Kustutamine
+    {
+        public void delete()
+        {
+            Console.WriteLine(@"Mis on path, kus failid on? (See peab lõppema folderiga, näiteks E:\TestFolder)");
+            string path = Console.ReadLine() + @"\";
+            Console.WriteLine("Mis faili tüüpi kustutada? (.srt, .txt, .jpeg)");
+            string failitüüp = Console.ReadLine();
+            string[] AllFiles = Directory.GetFiles(path, "*" + failitüüp);
+
+            foreach (string file in AllFiles)
+            {
+                File.Delete(file);
+            }
+        }
+    }
+}
--- TelesarjadeRenameimine/Program.cs
+++ TelesarjadeRenameimine/Program.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 using System.IO;
 
 //TODO
-//* Kustutada ühte tüüpi failid(Kuradi subtiitlid ja tekst failid)
+//* Kustutada ühte tüüpi failid(Kuradi subtiitlid ja tekst failid) [TEHTUD]
 //* Ainult renameida ühte faili tüüpi(mp4, mkv või mis iganes user sisestab)
 //* Folderitest kõik asjad välja tuua
 
@@ -17,13 +17,41 @@ namespace TelesarjadeRenameimine
         static void Main(string[] args)
         {
             TaasNimetamine Nimetamine = new TaasNimetamine();
+            Kustutamine kustuta = new Kustutamine();
             string lõpetada = "Mdea";
-
-            while (lõpetada != "stop" || lõpetada != "stopp")
+            Console.WriteLine("Mida tahad teha? rename/kustuta/mõlemat");
+            string vastus = Console.ReadLine();
+            if (vastus == "rename")
             {
-                Nimetamine.rename();
-                Console.WriteLine("Kui tahad lõpetada kirjuta stop, muidu kirjuta mis iganes muud");
-                lõpetada = Console.ReadLine();
+                while (lõpetada != "stop" || lõpetada != "stopp")
+                {
+                    Nimetamine.rename();
+                    Console.WriteLine("Kui tahad lõpetada kirjuta stop, muidu kirjuta mis iganes muud");
+                    lõpetada = Console.ReadLine();
+                }
+            }
+            else if (vastus == "kustuta")
+            {
+                while (lõpetada != "stop" || lõpetada != "stopp")
+                {
+                    kustuta.delete();
+                    Console.WriteLine("Kui tahad lõpetada kirjuta stop, muidu kirjuta mis iganes muud");
+                    lõpetada = Console.ReadLine();
+                }
+            }
+            else if (vastus == "mõlemat")
+            {
+                while (lõpetada != "stop" || lõpetada != "stopp")
+                {
+                    kustuta.delete();
+                    Nimetamine.rename();
+                    Console.WriteLine("Kui tahad lõpetada kirjuta stop, muidu kirjuta mis iganes muud");
+                    lõpetada = Console.ReadLine();
+                }
+            }
+            else
+            {
+                Console.WriteLine("Õpi juhendeid lugema");
             }
         }
     }
